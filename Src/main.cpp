769a02cf9242c975baaa018831ffcbde41c3c527
systas0 <shibadaigogo@gmail.com>
2020-05-05 23:59:47 +0900

    ファイル、ディレクトリ追加

---
+++ Src/main.cpp
@@ -0,0 +1,15 @@
+
+//F405ControlBoard* controlBoard;
+
+int main(void)
+{
+    //ペリフェラルセットアップ
+    //controlBoard->setupPeripheral();
+    //インタフェースセットアップ(LED、ボタン、ユーザエンコーダ、LCD、PSコンx2、エンコーダ)
+    //controlBoard->setupInterface();
+
+    while(1)
+    {
+        //controlBoard->cycle();
+    }
+}
